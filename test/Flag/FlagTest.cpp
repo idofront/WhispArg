78d0197fa92cc9e817e15582a06ae6a887a9e4cf
Kumazawa <kumazawa@ic.idofront.com>
2024-12-22 12:36:51 +0900

    Add Unit Tests and Update Actions Configuration

---
+++ test/Flag/FlagTest.cpp
@@ -0,0 +1,32 @@
+#include <gtest/gtest.h>
+#include <idofront/WhispArg.hpp>
+
+using namespace idofront::argument::type;
+
+TEST(FlagTest, DefaultConstructorShouldBeFalse)
+{
+    Flag f;
+    EXPECT_FALSE(f) << "Default-constructed Flag should be false.";
+    EXPECT_EQ("false", f.ToString()) << "Expected string representation to be 'false'.";
+}
+
+TEST(FlagTest, ConstructorTrueShouldBeTrue)
+{
+    Flag f(true);
+    EXPECT_TRUE(f) << "Flag constructed with true should evaluate to true.";
+    EXPECT_EQ("true", f.ToString()) << "Expected string representation to be 'true'.";
+}
+
+TEST(FlagTest, StaticFlagTrueIsTrue)
+{
+    Flag f = Flag::True;
+    EXPECT_TRUE(f) << "Flag::True should evaluate to true.";
+    EXPECT_EQ("true", f.ToString()) << "Expected string representation to be 'true'.";
+}
+
+TEST(FlagTest, StaticFlagFalseIsFalse)
+{
+    Flag f = Flag::False;
+    EXPECT_FALSE(f) << "Flag::False should evaluate to false.";
+    EXPECT_EQ("false", f.ToString()) << "Expected string representation to be 'false'.";
+}
