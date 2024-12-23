bfff1cf75bee73170c6251c91eaecc2f011d98d3
Kumazawa <kumazawa@ic.idofront.com>
2024-12-24 00:01:29 +0900

    WhispArgExample に実装者向けコメントを追加，加えて WhipArg で Flag 型を特殊扱いするよう修正

--- include/idofront/WhispArg.hpp
+++ include/idofront/WhispArg.hpp
@@ -59,12 +59,12 @@ namespace type
 class Flag
 {
   public:
-    Flag(bool value = false) : _Value(value)
+    constexpr Flag(bool value = false) : _Value(value)
     {
     }
 
     /// @brief Converts the Flag class to a bool.
-    operator bool() const
+    constexpr operator bool() const
     {
         return _Value;
     }
@@ -189,8 +189,6 @@ template <typename T> class Argument
         std::transform(upperCaseName.begin(), upperCaseName.end(), upperCaseName.begin(),
                        [](char const &c) { return std::toupper(c); });
 
-        auto isFlag = std::is_same_v<T, type::Flag>;
-
         std::stringstream ss;
         ss << "  ";
         if (!_ShortName.empty())
@@ -198,7 +196,7 @@ template <typename T> class Argument
             ss << "-" << _ShortName << ", ";
         }
         ss << "--" << _Name;
-        if (!isFlag)
+        if constexpr (!std::is_same_v<T, type::Flag>)
         {
             ss << " <" << upperCaseName << ">";
         }
@@ -222,6 +220,10 @@ template <typename T> class Argument
     Argument(const std::string &shortName, const std::string &name)
         : _Name(name), _ShortName(shortName), _Description(), _DefaultValue(std::nullopt), _IsRequired(false)
     {
+        if constexpr (std::is_same_v<T, type::Flag>)
+        {
+            Default(type::Flag::False);
+        }
     }
 
     Argument(const std::string &name) : Argument("", name)
@@ -339,7 +341,7 @@ template <typename T> std::optional<T> Parse(std::vector<std::string> argv, cons
                   "Type not supported automatically. Please provide a converter function.");
 
     auto getConverter = std::function<std::function<T(const std::string &)>(const Argument<T> &)>(
-        [](const Argument<T> &) -> std::function<T(const std::string &)> {
+        [](const Argument<T> &argument) -> std::function<T(const std::string &)> {
             if constexpr (std::is_same_v<T, int8_t> || std::is_same_v<T, int16_t> || std::is_same_v<T, int32_t> ||
                           std::is_same_v<T, int64_t>)
             {
@@ -393,7 +395,7 @@ template <typename T> std::optional<T> Parse(std::vector<std::string> argv, cons
             }
             else if constexpr (std::is_same_v<T, type::Flag>)
             {
-                return [](const std::string &) { return type::Flag::True; };
+                return [&argument](const std::string &) { return !argument.Value().value(); };
             }
             else
             {
--- main/WhispArgExample.cpp
+++ main/WhispArgExample.cpp
@@ -26,6 +26,7 @@ int main(int argc, char *argv[])
 {
     try
     {
+        // step 1: Create a parser object.
         auto parser =
             idofront::whisparg::WhispArg(argc, argv)
                 .Description("An example of WhispArg.\n"
@@ -35,6 +36,9 @@ int main(int argc, char *argv[])
                 .Name("WhispArgExample")
                 .Version("v0.0.0");
 
+        // step 2: Create argument and use to parse.
+        // step 2.1: Create arguments.
+        // Note: The description can be written in multiple lines by specifying a line feed code in the Description.
         auto helpWidthArgument =
             idofront::whisparg::Argument<uint16_t>::New('w', "help-width")
                 .Description(
@@ -49,37 +53,44 @@ int main(int argc, char *argv[])
                     "saucer of milk.\"\n"
                     "(My Father's Dragon by Ruth Stiles Gannett)")
                 .Default(80);
-        auto helpWidth = parser.Parse(helpWidthArgument);
-
+        // Note: With the Flag type, you can define a flag that does not take an argument. When a flag is specified in
+        // the command line argument, the boolean value specified by Default() is inverted. If Default() is not
+        // specified, the Flag type will be false.
         auto noDescriptionArgument =
-            idofront::whisparg::Argument<idofront::whisparg::type::Flag>::New('n', "no-description")
-                .Default(idofront::whisparg::type::Flag::False);
-        auto noDescription = parser.Parse(noDescriptionArgument);
-
+            idofront::whisparg::Argument<idofront::whisparg::type::Flag>::New('n', "no-description");
         auto lengthArgument = idofront::whisparg::Argument<uint8_t>::New('l', "length")
                                   .Description("The length of the manager.")
                                   .Default(1);
-        auto length = parser.Parse(lengthArgument);
-
         auto messageArgument = idofront::whisparg::Argument<std::string>::New("message")
                                    .Description("The message to be published.")
                                    .Default("Hello, world!");
+
+        // stem 2.2: Parse arguments.
+        auto helpWidth = parser.Parse(helpWidthArgument);
+        auto noDescription = parser.Parse(noDescriptionArgument);
+        auto length = parser.Parse(lengthArgument);
         auto message = parser.Parse(messageArgument);
 
+        // Note: The Help argument is provided as a preset. Is is not necessary to use it. You can define your own if
+        // you want.
         auto help = parser.Parse<idofront::whisparg::type::Flag>(idofront::whisparg::Help);
 
+        // step 3: Use the parsed arguments.
+        // step 3.1: Show help message.
+        // Note: It is recommended to process the help message before parsing other arguments.
         if (help.Value().value())
         {
             parser.ShowHelp(helpWidth.Value().value());
             return 0;
         }
+
+        // step 3.2: Process the arguments you are interested in.
         if (noDescription.Value().value())
         {
             std::cout << "The \"--no-description\" is example of no description argument on the help message.\n"
                       << std::flush;
             return 0;
         }
-
         std::cout << "Length: " << (uint64_t)length.Value().value() << "\n" << std::flush;
         std::cout << "Message: " << message.Value().value() << "\n" << std::flush;
     }
